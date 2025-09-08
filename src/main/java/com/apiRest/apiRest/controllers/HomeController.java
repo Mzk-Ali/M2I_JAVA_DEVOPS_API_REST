bc4e97a683ed36f6555d939b4d5e1ee18aef2936
Mzk-Ali <161448982+Mzk-Ali@users.noreply.github.com>
2025-09-08 13:38:57 +0200

    Initial Commit

---
+++ src/main/java/com/apiRest/apiRest/controllers/HomeController.java
@@ -0,0 +1,17 @@
+package com.apiRest.apiRest.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api")
+public class HomeController {
+	
+	
+	@GetMapping("")
+	public String index() {
+		return "Hello";
+	}
+}
